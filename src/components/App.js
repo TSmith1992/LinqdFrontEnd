21500018f672acf6f913735c59e1359c44dcf760
Tarique Smith <tpsmith1992@gmail.com>
2021-11-30 16:05:36 -0500

    Began moving App file to components folder

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://m.media-amazon.com/images/I/51GWpiHp6YL._AC_.jpg'
+        alt="spiderweb logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
