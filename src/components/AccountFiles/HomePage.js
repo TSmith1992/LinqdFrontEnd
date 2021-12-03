0edc53dd35ca4a7ce4c98cdbe7ee159bc84e50ee
Tarique Smith <tpsmith1992@gmail.com>
2021-12-03 13:01:24 -0500

    Further Hpage testing

--- src/components/AccountFiles/HomePage.js
+++ src/components/AccountFiles/HomePage.js
@@ -4,6 +4,10 @@ export default function HomePage({setCurrentUser, currentUser}) {
     return (
         <div>
             "Hi! I'm the HP!" {currentUser.name}
+            <img 
+            src={currentUser.picture}
+            alt='Who I am'
+            />
         </div>
     )
 }
